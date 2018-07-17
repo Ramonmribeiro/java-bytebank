35de3d4b9f56c1008a7d54fc6cb9c162f04a9a59
ramonmribeiro <ramon.mribeiro@yahoo.com.br>
2018-07-17 07:25:26 -0300

    Commit 17/07/2018

--- src/bytebank/Conta.java
+++ src/bytebank/Conta.java
@@ -6,7 +6,25 @@ public class Conta {
 	int numero;
 	String titulo;
 	
-	void deposita(double valor ){
-		this.saldo = this.saldo + valor;
+	public void deposita(double valor ){
+		this.saldo += valor;
+	}
+	
+	public boolean saca(double valor){
+		if(this.saldo >= valor){
+			this.saldo -= valor;
+			return true;
+		}else{
+			return false;
+		}
+	}
+	
+	public boolean transfere(double valor, Conta destino){
+		if(this.saldo >= valor){
+			this.saldo -= valor;
+			destino.deposita(valor);
+			return true;
+		}
+		return false;
 	}
 }
--- src/bytebank/TestaMetodo.java
+++ src/bytebank/TestaMetodo.java
@@ -3,10 +3,26 @@ package bytebank;
 public class TestaMetodo {
 
 	public static void main(String[] args) {
-		Conta conta = new Conta();
-		conta.saldo = 100;
-		conta.deposita(50);
-		System.out.println(conta.saldo);
+		Conta contaDoPaulo = new Conta();
+		contaDoPaulo.saldo = 100;
+		contaDoPaulo.deposita(50);
+		System.out.println("Saldo inicio contaDoPaulo: " + contaDoPaulo.saldo);
+		
+		boolean conseguiuRetirar = contaDoPaulo.saca(200);
+		System.out.println(conseguiuRetirar);
+		System.out.println("Novo saldo contaDoPaulo: " + contaDoPaulo.saldo);
+		
+		Conta contaDaMarcela = new Conta();
+		contaDaMarcela.deposita(1000);
+		System.out.println("Primeiro saldo contaDaMarcela: " + contaDaMarcela.saldo);
+		
+		if(contaDaMarcela.transfere(1300, contaDoPaulo)){
+			System.out.println("transferencia feita com sucesso");
+		}else{
+			System.out.println("transferencia não realizada, faltou saldo");
+		}
+		System.out.println("Saldo contaDaMarcela: " + contaDaMarcela.saldo);
+		System.out.println("Saldo contaDoPaulo: " + contaDoPaulo.saldo);
 
 	}
 
